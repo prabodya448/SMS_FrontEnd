f717c98a21b8a5efb1ede36b9b5395ec8d3a0fa1
prabodya <116181860+prabodya448@users.noreply.github.com>
2024-11-11 11:48:55 +0530

    1.2

--- src/app/components/forms/fuser-management/fuser-management.component.ts
+++ src/app/components/forms/fuser-management/fuser-management.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { SearchBottonComponent } from "../../search-botton/search-botton.component";
 import { UserManagementComponent } from "../../search list/user-management/user-management.component";
+import { User } from '../../../model/class/User';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-fuser-management',
   standalone: true,
-  imports: [SearchBottonComponent, UserManagementComponent],
+  imports: [SearchBottonComponent, UserManagementComponent,FormsModule],  //form for post
   templateUrl: './fuser-management.component.html',
   styleUrl: './fuser-management.component.css'
 })
 export class FuserManagementComponent {
+  userObj : User =  new User();   //post
 
+  userList: User[]=[];   //post
 }
---
+++ src/app/model/class/User.ts
@@ -0,0 +1,22 @@
+export class User{
+    userID : number;
+    userName : string;
+    userRole : string;
+    userDOB : string;
+    userNic : string;
+    userEmail : string;
+    userContact : string;
+    userAge : string ;
+
+
+    constructor(){
+        this.userID=0;
+        this.userName='';
+        this.userRole='';
+        this.userDOB='';
+        this.userNic='';
+        this.userEmail='';
+        this.userContact='';
+        this.userAge='';
+    }
+}
